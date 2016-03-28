a1c5215b015f2efa9dfd1253a399ff984c39b8fd
KyleBrown <kbrown1993.kb@gmail.com>
2016-03-28 17:26:46 +0100

    Fixed qmake QT compatibility error

--- CherokeeTestGUI/ui_mainwindow.h
+++ CherokeeTestGUI/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.5.1
+** Created by: Qt User Interface Compiler version 4.8.6
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,17 +10,17 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QHeaderView>
-#include <QtWidgets/QLCDNumber>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QSlider>
-#include <QtWidgets/QVBoxLayout>
-#include <QtWidgets/QWidget>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLCDNumber>
+#include <QtGui/QLabel>
+#include <QtGui/QMainWindow>
+#include <QtGui/QSlider>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
 
 QT_BEGIN_NAMESPACE
 
@@ -52,28 +52,28 @@ public:
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
-            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(693, 206);
         centralWidget = new QWidget(MainWindow);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         widget = new QWidget(centralWidget);
-        widget->setObjectName(QStringLiteral("widget"));
+        widget->setObjectName(QString::fromUtf8("widget"));
         widget->setGeometry(QRect(12, 22, 681, 171));
         verticalLayout_4 = new QVBoxLayout(widget);
         verticalLayout_4->setSpacing(6);
         verticalLayout_4->setContentsMargins(11, 11, 11, 11);
-        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
         verticalLayout_4->setContentsMargins(0, 0, 0, 0);
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setSpacing(6);
-        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
         label = new QLabel(widget);
-        label->setObjectName(QStringLiteral("label"));
+        label->setObjectName(QString::fromUtf8("label"));
 
         horizontalLayout->addWidget(label);
 
         lcdNumber = new QLCDNumber(widget);
-        lcdNumber->setObjectName(QStringLiteral("lcdNumber"));
+        lcdNumber->setObjectName(QString::fromUtf8("lcdNumber"));
 
         horizontalLayout->addWidget(lcdNumber);
 
@@ -82,12 +82,12 @@ public:
 
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setSpacing(6);
-        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         verticalLayout_3 = new QVBoxLayout();
         verticalLayout_3->setSpacing(6);
-        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
         PitchCommand = new QSlider(widget);
-        PitchCommand->setObjectName(QStringLiteral("PitchCommand"));
+        PitchCommand->setObjectName(QString::fromUtf8("PitchCommand"));
         PitchCommand->setMinimumSize(QSize(400, 0));
         PitchCommand->setMaximumSize(QSize(400, 16777215));
         PitchCommand->setMinimum(-20);
@@ -97,7 +97,7 @@ public:
         verticalLayout_3->addWidget(PitchCommand);
 
         RollCommand = new QSlider(widget);
-        RollCommand->setObjectName(QStringLiteral("RollCommand"));
+        RollCommand->setObjectName(QString::fromUtf8("RollCommand"));
         RollCommand->setMinimumSize(QSize(400, 0));
         RollCommand->setMaximumSize(QSize(400, 16777215));
         RollCommand->setMinimum(-20);
@@ -107,7 +107,7 @@ public:
         verticalLayout_3->addWidget(RollCommand);
 
         YawRateCommand = new QSlider(widget);
-        YawRateCommand->setObjectName(QStringLiteral("YawRateCommand"));
+        YawRateCommand->setObjectName(QString::fromUtf8("YawRateCommand"));
         YawRateCommand->setMinimumSize(QSize(400, 0));
         YawRateCommand->setMaximumSize(QSize(400, 16777215));
         YawRateCommand->setMinimum(-20);
@@ -121,19 +121,19 @@ public:
 
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setSpacing(6);
-        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
         label_6 = new QLabel(widget);
-        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setObjectName(QString::fromUtf8("label_6"));
 
         verticalLayout_2->addWidget(label_6);
 
         label_3 = new QLabel(widget);
-        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setObjectName(QString::fromUtf8("label_3"));
 
         verticalLayout_2->addWidget(label_3);
 
         label_4 = new QLabel(widget);
-        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setObjectName(QString::fromUtf8("label_4"));
 
         verticalLayout_2->addWidget(label_4);
 
@@ -142,23 +142,23 @@ public:
 
         verticalLayout = new QVBoxLayout();
         verticalLayout->setSpacing(6);
-        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         ElevatorOut = new QLabel(widget);
-        ElevatorOut->setObjectName(QStringLiteral("ElevatorOut"));
+        ElevatorOut->setObjectName(QString::fromUtf8("ElevatorOut"));
         ElevatorOut->setEnabled(true);
         ElevatorOut->setMinimumSize(QSize(30, 0));
-        ElevatorOut->setToolTipDuration(0);
+        //ElevatorOut->setToolTipDuration(0);
 
         verticalLayout->addWidget(ElevatorOut);
 
         AileronOut = new QLabel(widget);
-        AileronOut->setObjectName(QStringLiteral("AileronOut"));
+        AileronOut->setObjectName(QString::fromUtf8("AileronOut"));
         AileronOut->setMinimumSize(QSize(30, 0));
 
         verticalLayout->addWidget(AileronOut);
 
         RudderOut = new QLabel(widget);
-        RudderOut->setObjectName(QStringLiteral("RudderOut"));
+        RudderOut->setObjectName(QString::fromUtf8("RudderOut"));
         RudderOut->setMinimumSize(QSize(30, 0));
 
         verticalLayout->addWidget(RudderOut);
@@ -167,12 +167,12 @@ public:
         horizontalLayout_2->addLayout(verticalLayout);
 
         label_5 = new QLabel(widget);
-        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setObjectName(QString::fromUtf8("label_5"));
 
         horizontalLayout_2->addWidget(label_5);
 
         label_2 = new QLabel(widget);
-        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setObjectName(QString::fromUtf8("label_2"));
 
         horizontalLayout_2->addWidget(label_2);
 
@@ -190,14 +190,14 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
-        label->setText(QApplication::translate("MainWindow", "Gyro Rate Z", 0));
-        label_6->setText(QApplication::translate("MainWindow", "Elevator Deflection:", 0));
-        label_3->setText(QApplication::translate("MainWindow", "Aileron Deflection:", 0));
-        label_4->setText(QApplication::translate("MainWindow", "Rudder Defecection:", 0));
-        ElevatorOut->setText(QApplication::translate("MainWindow", "0", 0));
-        AileronOut->setText(QApplication::translate("MainWindow", "0", 0));
-        RudderOut->setText(QApplication::translate("MainWindow", "0", 0));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("MainWindow", "Gyro Rate Z", 0, QApplication::UnicodeUTF8));
+        label_6->setText(QApplication::translate("MainWindow", "Elevator Deflection:", 0, QApplication::UnicodeUTF8));
+        label_3->setText(QApplication::translate("MainWindow", "Aileron Deflection:", 0, QApplication::UnicodeUTF8));
+        label_4->setText(QApplication::translate("MainWindow", "Rudder Defecection:", 0, QApplication::UnicodeUTF8));
+        ElevatorOut->setText(QApplication::translate("MainWindow", "0", 0, QApplication::UnicodeUTF8));
+        AileronOut->setText(QApplication::translate("MainWindow", "0", 0, QApplication::UnicodeUTF8));
+        RudderOut->setText(QApplication::translate("MainWindow", "0", 0, QApplication::UnicodeUTF8));
         label_5->setText(QString());
         label_2->setText(QString());
     } // retranslateUi
